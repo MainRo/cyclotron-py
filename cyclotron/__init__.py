9d7f65b5b499f17877d9c4ed5bdad4c8d898a1d1
Romain Picard <romain.picard@oakbits.com>
2019-06-23 22:32:42 +0200

    added support of bumpversion

--- cyclotron/__init__.py
+++ cyclotron/__init__.py
@@ -1,5 +1,9 @@
 from collections import namedtuple
 
+__author__ = """Romain Picard"""
+__email__ = 'romain.picard@oakbits.com'
+__version__ = '0.6.0'
+
 Component = namedtuple('Component', ['call', 'input', 'output'])
 Component.__new__.__defaults__ = (None, None)
 
