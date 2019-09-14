8e829e3258040e874c1410d7b1498c2851b0a5df
Romain Picard <romain.picard@oakbits.com>
2019-09-14 21:46:42 +0200

    added asyncio

---
+++ cyclotron/asyncio/runner.py
@@ -0,0 +1,14 @@
+import asyncio
+from cyclotron.rx import setup
+
+def run(entry_point, drivers, loop=None):
+    ''' This is a runner wrapping the cyclotron "run" implementation. It takes
+    an additional parameter to provide a custom asyncio mainloop.
+    '''
+    program = setup(entry_point, drivers)
+    dispose = program.run()
+    if loop is None:
+        loop = asyncio.get_event_loop()
+
+    loop.run_forever()
+    dispose()
---
+++ cyclotron/asyncio/stop.py
@@ -0,0 +1,40 @@
+from collections import namedtuple
+from cyclotron import Component
+import asyncio
+
+Sink = namedtuple('Sink', ['control'])
+
+
+def make_driver(loop=None):
+    ''' Returns a stop driver. 
+
+    The optional loop argument can be provided to use the driver in another
+    loop than the default one.
+
+    Parameters
+    -----------
+    loop: BaseEventLoop
+        The event loop to use instead of the default one.
+    '''
+    loop = loop or asyncio.get_event_loop()
+
+    def stop(i=None):
+        loop.stop()
+
+    def driver(sink):
+        ''' The stop driver stops the asyncio event loop.
+        The event loop is stopped as soon as an event is received on the
+        control observable or when it completes (both in case of success or
+        error).
+
+        Parameters
+        ----------
+            sink: Sink
+        '''
+        sink.control.subscribe(
+            on_next=stop,
+            on_error=stop,
+            on_completed=stop)
+        return None
+
+    return Component(call=driver, input=Sink)
